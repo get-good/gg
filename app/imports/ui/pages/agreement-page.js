f1666a7779eb6f7fc338edd08271b77a6f27090d
akuloloia <akuloloi@hawaii.edu>
2016-11-03 09:12:47 -1000

    Update agreement-page.js

--- app/imports/ui/pages/agreement-page.js
+++ app/imports/ui/pages/agreement-page.js
@@ -1,9 +1,9 @@
 Template.Agreement_Page.events({
-  'click .agree'(event) {
+  'submit .agree'(event) {
     event.preventDefault();
     FlowRouter.go('User_Home_Page');
   },
-  'click .disagree'(event) {
+  'submit .disagree'(event) {
     event.preventDefault();
     FlowRouter.go('Public_Landing_Page');
   },
