5ce3d5a6c4801dd7c05081283f10d50da14a3431
Dylan Pavao <dppavao@hawaii.edu>
2016-11-27 17:54:13 -1000

    fixed home page
    Issue where home page would not redirect user to the rating and edit
    page

--- app/imports/ui/pages/user-home-page.js
+++ app/imports/ui/pages/user-home-page.js
@@ -9,6 +9,20 @@ Template.User_Home_Page.events({
   },
 });
 
+Template.User_Home_Page.events({
+  'click .rating'(event) {
+    event.preventDefault();
+    FlowRouter.go('rating');
+  },
+});
+
+Template.User_Home_Page.events({
+  'click .edit'(event) {
+    event.preventDefault();
+    FlowRouter.go('Edit_Profile');
+  },
+});
+
 // Template.User_Home_Page.check = function(){
 //   setTimeout(function() {
 //     alert('This is your home page! From here, you can edit your profile,read your about page and even check your current helper rating. When you\'re ready to explore, feel free to click any of the links up top!');
